82f459f405c534561d2d409ef9303d9a872e6f59
kc-noorashrafi <kc-noorashrafi@kcaws1wts08.kcapps.kpmg.com>
2016-08-02 13:01:05 +0000

    updated

--- src/main/java/com/codependent/storyteller/ConfigServerApplication.java
+++ src/main/java/com/codependent/storyteller/ConfigServerApplication.java
@@ -1,34 +1,19 @@
 package com.codependent.storyteller;
 
-import java.net.URISyntaxException;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.configuration.AbstractConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.config.server.config.EnvironmentRepositoryConfiguration;
-import org.springframework.cloud.config.server.environment.EnvironmentRepository;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClientException;
-
-import com.netflix.config.ConcurrentMapConfiguration;
-import com.netflix.config.DynamicLongProperty;
-import com.netflix.config.DynamicPropertyFactory;
-import com.netflix.config.DynamicStringProperty;
 
 @EnableConfigServer
 @EnableEurekaClient
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ConfigServerApplication {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
@@ -41,8 +26,8 @@ public class ConfigServerApplication {
 	@Autowired
 	EnvironmentRepositoryConfiguration config;
 
-	@Autowired
-	EnvironmentRepository repo;
+	/*@Autowired
+	EnvironmentRepository repo;*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
@@ -61,10 +46,10 @@ public class ConfigServerApplication {
 		// "config.properties");
 	}
 
-	private void logConfig() {
+	/*private void logConfig() {
 		config.configServerHealthIndicator(repo).health().getDetails();
 	}
-
+*/
 	/*
 	 * @Bean public AbstractConfiguration sampleArchaiusConfiguration() throws
 	 * Exception { ConcurrentMapConfiguration concurrentMapConfiguration = new
